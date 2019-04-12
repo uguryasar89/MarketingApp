222ab5c8c6734f4c242ad4c0d52731b9fd454fae
Uğur Yaşar <uguryasar89@gmail.com>
2019-04-12 21:16:15 +0300

    Kullanılan annotation'ların açıklamaları eklendi.

--- marketing-controller/src/main/java/com/uguryasar/edu/marketing/controller/config/ControllerConfig.java
+++ marketing-controller/src/main/java/com/uguryasar/edu/marketing/controller/config/ControllerConfig.java
@@ -3,7 +3,7 @@ package com.uguryasar.edu.marketing.controller.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.uguryasar.edu.marketing")
+@SpringBootApplication(scanBasePackages = "com.uguryasar.edu.marketing") // Bu projeyi Spring Boot uygulaması yapar.
 public class ControllerConfig {
 
     public static void main(String[] args) {
--- marketing-controller/src/main/java/com/uguryasar/edu/marketing/controller/rest/HomeController.java
+++ marketing-controller/src/main/java/com/uguryasar/edu/marketing/controller/rest/HomeController.java
@@ -4,15 +4,15 @@ import com.uguryasar.edu.marketing.controller.dto.StatusDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // Bu sınıfın metodlarının Rest servisi yapar.
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/") // Bu metoda "/" adresi ile erişilmesini sağlar.
     public String welcome() {
         return "Welcome to MarketingApp";
     }
 
-    @GetMapping("/status")
+    @GetMapping("/status") // Bu metoda /status adresi ile erişilmesini sağlar
     public StatusDto status() {
         StatusDto status = new StatusDto();
 
